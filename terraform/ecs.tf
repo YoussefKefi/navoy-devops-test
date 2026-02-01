f1a841ee3df1b5a102c95646d18811540577fb60
Youssef Kefi <youssef.kefi@esprit.tn>
2026-02-01 20:07:45 +0100

    added ECS autoscaling (2-10 containers)

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -171,3 +171,30 @@ resource "aws_ecs_service" "app" {
     Environment = var.environment
   }
 }
+
+# Auto Scaling Target
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 10
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.app.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+# Auto Scaling Policy - CPU Based
+resource "aws_appautoscaling_policy" "scale_cpu" {
+  name               = "${var.project_name}-scale-cpu"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value       = 70.0
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 60
+  }
+}
